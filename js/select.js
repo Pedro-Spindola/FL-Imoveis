7dd80e4a2eea37564ba56757b981d23fad205f92
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2024-05-22 22:52:35 -0300

    Update slide tela select final

--- js/select.js
+++ js/select.js
@@ -1,29 +1,49 @@
 const imgs = document.getElementById("container-img");
 const img = document.querySelectorAll("#container-img img");
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
 
 let idx = 0;
 
-function carrosel(){
+function showNextImage() {
     idx++;
     if (window.innerWidth <= 999 && window.innerWidth > 768) {
-        if(idx > img.length - 2){
+        if (idx > img.length - 2) {
             idx = 0;
         }
-        imgs.style.transform = `translateX(${-idx * (100 / 2)}vw)`;
+        imgs.style.transform = `translateX(${-idx * (100 / 3)}vw)`;
     } else if (window.innerWidth <= 768) {
-        if(idx > img.length - 1){
+        if (idx > img.length - 1) {
             idx = 0;
         }
         imgs.style.transform = `translateX(${-idx * 100}vw)`;
-    }else{
-        if(idx > img.length - 3){
+    } else {
+        if (idx > img.length - 3) {
             idx = 0;
         }
-
         imgs.style.transform = `translateX(${-idx * (100 / 3)}vw)`;
     }
-    
-    window.addEventListener('resize', carrosel);
 }
 
-setInterval(carrosel, 2500);
+function showPrevImage() {
+    idx--;
+    if (window.innerWidth <= 999 && window.innerWidth > 768) {
+        if (idx < 0) {
+            idx = img.length - 3;
+        }
+        imgs.style.transform = `translateX(${-idx * (100 / 3)}vw)`;
+    } else if (window.innerWidth <= 768) {
+        if (idx < 0) {
+            idx = img.length - 1;
+        }
+        imgs.style.transform = `translateX(${-idx * 100}vw)`;
+    } else {
+        if (idx < 0) {
+            idx = img.length - 3;
+        }
+        imgs.style.transform = `translateX(${-idx * (100 / 3)}vw)`;
+    }
+}
+
+prevButton.addEventListener('click', showPrevImage);
+nextButton.addEventListener('click', showNextImage);
